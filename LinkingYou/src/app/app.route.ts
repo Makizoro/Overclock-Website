27d97eb14314db5b0f7f1ae104bb1eddba296804
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2020-09-02 20:04:30 +0200

    Created new routes

--- LinkingYou/src/app/app.route.ts
+++ LinkingYou/src/app/app.route.ts
@@ -6,13 +6,15 @@ import {NgModule} from '@angular/core';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {CsiTabsComponent} from './csi-tabs/csi-tabs.component';
 import {UserPageComponent} from './user-page/user-page.component';
+import {CsiPageComponent} from './csi-page/csi-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent },
   { path: 'sidebar', component: SidebarComponent, children: [
       { path: 'csi', component: CsiTabsComponent, outlet: 'routerSidebar'},
-      { path: 'userPage', component: UserPageComponent, outlet: 'routerSidebar'},
+      { path: 'userPage/:name', component: UserPageComponent, outlet: 'routerSidebar'},
+      { path: 'csiPage/:name', component: CsiPageComponent, outlet: 'routerSidebar'},
       { path: '', component: WelcomePageComponent, pathMatch: 'full', outlet: 'routerSidebar'}
     ]},
   { path: '', redirectTo: '/login', pathMatch: 'full' }
