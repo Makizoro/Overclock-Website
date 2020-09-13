1d621f65226353bddb1ae6821a5b40d3d51dc313
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2020-09-13 16:40:50 +0200

    Displays CSIs now

--- LinkingYou/src/app/csi-tabs/csi-tabs.component.ts
+++ LinkingYou/src/app/csi-tabs/csi-tabs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {CsiService} from '../services/csi.service'
 
 @Component({
@@ -15,15 +15,57 @@ export class CsiTabsComponent implements OnInit {
   // an onClick method that navigates to the csiPage (CSI profile page) with the csi name as a route parameter
   // TODO: Pull all CSI names and descriptions and store them in data in the following format:
   //  data: { csi: {name, description}, societies: {name, description}, interestGroups: {name, description}}
-  constructor(private csiService: CsiService) { }
+  constructor(private csiService: CsiService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
-    this.csiService.getCSI().subscribe(csi =>{
-    
+
+    this.csiService.getCSI().subscribe(csi => {
       this.data = csi;
       console.log(this.data);
-    });
+      this.displayCSIData(this.data);
 
+    });
+  }
+
+  navigateToCSI: EventListener = (e) => {
+    console.log(e);
+    const temp = e.composedPath()[1] as HTMLDivElement;
+    const csiName = temp.id;
+    console.log(csiName);
+    this.router.navigateByUrl('/sidebar/csiPage:' + csiName, );
+
+  }
+
+  displayCSIData(data): void{
+    const c = document.getElementById('clubList');
+    const s = document.getElementById('societyList');
+    const ig = document.getElementById('interestGroupList');
+
+    for (const e of data){
+      const newDiv = document.createElement('div');
+      newDiv.id = (e.name);
+      const csiTitle = document.createElement('h5');
+      csiTitle.innerHTML = e.name;
+      csiTitle.addEventListener('click', this.navigateToCSI);
+      newDiv.appendChild(csiTitle);
+      const csiDescription = document.createElement('p');
+      csiDescription.innerHTML = e.description;
+      csiDescription.addEventListener('click', this.navigateToCSI);
+      newDiv.appendChild(csiDescription);
+      switch (e.type){
+        case 'Club': {
+          c.appendChild(newDiv);
+          break;
+        }
+        case 'Society': {
+          s.appendChild(newDiv);
+          break;
+        }
+        case 'InterestGroup': {
+          ig.appendChild(newDiv);
+          break;
+        }
+      }
+    }
   }
 }
