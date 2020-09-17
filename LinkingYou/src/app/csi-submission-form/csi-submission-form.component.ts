9ad10d68c8341b6185922e044a9392bb71f6db84
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2020-09-17 21:39:17 +0200

    Added extra stuff

--- LinkingYou/src/app/csi-submission-form/csi-submission-form.component.ts
+++ LinkingYou/src/app/csi-submission-form/csi-submission-form.component.ts
@@ -12,4 +12,27 @@ export class CsiSubmissionFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  submitForm(): void{
+    const name = (document.getElementById('nameInput') as HTMLInputElement).value;
+    const typeClub = (document.getElementById('typeInputClub') as HTMLInputElement).checked;
+    const typeSociety = (document.getElementById('typeInputSociety') as HTMLInputElement).checked;
+    const typeInterestGroup = (document.getElementById('typeInputInterestGroup') as HTMLInputElement).checked;
+    let type = '';
+    if (typeClub){
+      type = (document.getElementById('typeInputClub') as HTMLInputElement).value;
+    } else if (typeSociety){
+      type = (document.getElementById('typeInputSociety') as HTMLInputElement).value;
+    } else if (typeInterestGroup){
+      type = (document.getElementById('typeInputInterestGroup') as HTMLInputElement).value;
+    }
+    const venue = (document.getElementById('venueInput') as HTMLInputElement).value;
+    const description = (document.getElementById('descriptionInput') as HTMLInputElement).value;
+
+    if (name === '' || type === '' || venue === '' || description === ''){
+      alert('You have not completed all fields. Please ensure that all fields are filled and checkboxes clicked');
+    } else {
+      
+    }
+  }
+
 }
