a2af7b515911bc890ce4cfcea7298d6a1e7b9181
Makizoro <39122438+Makizoro@users.noreply.github.com>
2020-09-21 16:35:15 +0200

    Test spec small changes

--- LinkingYou/src/app/login/login.component.spec.ts
+++ LinkingYou/src/app/login/login.component.spec.ts
@@ -1,14 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import {DebugElement} from '@angular/core'
 import { LoginComponent } from './login.component';
+import { By } from '@angular/platform-browser';
+import {Router} from '@angular/router';
+import {AuthService} from '../services/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [ AuthService, Router]
+      
     })
     .compileComponents();
   }));
@@ -16,10 +22,12 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
 });
