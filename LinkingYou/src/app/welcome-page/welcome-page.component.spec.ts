bb7c4bd0d4c8df1a5644d4b9c1d7d7d1744438e2
Makizoro <39122438+Makizoro@users.noreply.github.com>
2020-09-27 17:44:55 +0200

    Updated welcom page test

--- LinkingYou/src/app/welcome-page/welcome-page.component.spec.ts
+++ LinkingYou/src/app/welcome-page/welcome-page.component.spec.ts
@@ -1,10 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { from } from 'rxjs';
 
 import { WelcomePageComponent } from './welcome-page.component';
+
+import {DebugElement} from '@angular/core'
+import {By} from '@angular/platform-browser'
 
 describe('WelcomePageComponent', () => {
   let component: WelcomePageComponent;
   let fixture: ComponentFixture<WelcomePageComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +21,16 @@ describe('WelcomePageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WelcomePageComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a P tag of "welcome-page works!" ', () => {
+    expect(de.query(By.css('p')).nativeElement.innerText).toBe('welcome-page works!');
   });
 });
