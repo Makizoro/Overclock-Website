02c893b2072beaca454afa1e552cb9750cb55517
Makizoro <39122438+Makizoro@users.noreply.github.com>
2020-09-27 17:11:03 +0200

    Alongside  previous commit

--- LinkingYou/src/app/services/subscription.service.spec.ts
+++ LinkingYou/src/app/services/subscription.service.spec.ts
@@ -2,11 +2,29 @@ import { TestBed } from '@angular/core/testing';
 
 import { SubscriptionService } from './subscription.service';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {DebugElement} from '@angular/core'
+import { By } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { config} from '../app.module'
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { of } from 'rxjs/internal/observable/of';
+import { AppRouteModule } from '../app.route';
+
 describe('SubscriptionService', () => {
   let service: SubscriptionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[ 
+        AngularFireModule.initializeApp(config),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        AppRouteModule
+      ]
+    });
     service = TestBed.inject(SubscriptionService);
   });
 
